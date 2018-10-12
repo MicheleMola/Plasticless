da48ddb4864ff723fb9b18cd9337b79d10659f63
Michele Mola <michelemola@micheles-macbook-pro.local>
2018-10-12 16:31:06 +0200

    Initial Commit

---
+++ Plasticless/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Plasticless
+//
+//  Created by Michele Mola on 10/12/18.
+//  Copyright © 2018 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Plasticless/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Plasticless
+//
+//  Created by Michele Mola on 10/12/18.
+//  Copyright © 2018 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
