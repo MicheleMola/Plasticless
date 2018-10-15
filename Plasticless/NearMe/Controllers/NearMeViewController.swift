cb64d6a1634b0fef19d2c65190aa0e31420ffb12
Emilio Arvonio <emilioarvonio@macbook-pro-111.local>
2018-10-15 17:57:43 +0200

    Create le View della sezione Near Me. Una schermata con un segmented controller, una con la mappa e l'ultima con la table view dinamica. Sono state create anche i controller per ogni view.

---
+++ Plasticless/NearMe/Controllers/NearMeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NearMeViewController.swift
+//  Plasticless
+//
+//  Created by Emilio Arvonio on 15/10/2018.
+//  Copyright © 2018 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class NearMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
