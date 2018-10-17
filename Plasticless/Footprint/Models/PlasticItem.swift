96ecfc27157ee031f66272d151453248ef64d5cd
Luigi Barretta <luigi.barretta@capgemini.com>
2018-10-17 14:34:49 +0200

    Modifiche alla table view effettuate.

---
+++ Plasticless/Footprint/Models/PlasticItem.swift
@@ -0,0 +1,15 @@
+//
+//  PlasticItem.swift
+//  Plasticless
+//
+//  Created by Luigi Barretta on 16/10/2018.
+//  Copyright © 2018 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+struct PlasticItem {
+    let image: UIImage
+    let name: String
+    let quantity: Int
+}
